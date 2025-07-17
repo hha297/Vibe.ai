5dc2acfe262d566a4f2d220c7ee63d5c3e5c42da
hoangha297 <ha.dh290702@gmail.com>
2025-07-17 11:14:56 +0300

    E2B Sandbox

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,21 @@
 import { inngest } from './client';
-
+import { Sandbox } from '@e2b/code-interpreter';
+import { getSandbox } from './utils';
 export const helloWorld = inngest.createFunction(
         { id: 'hello-world' },
         { event: 'test/hello.world' },
         async ({ event, step }) => {
-                await step.sleep('wait-a-moment', '1s');
-                return { message: `Hello ${event.data.email}!` };
+                const sanboxId = await step.run('get-sandbox-id', async () => {
+                        const sandbox = await Sandbox.create('vibe-nextjs-hha297');
+                        return sandbox.sandboxId;
+                });
+
+                const sandboxUrl = await step.run('get-sandbox-url', async () => {
+                        const sandbox = await getSandbox(sanboxId);
+                        const host = sandbox.getHost(3000);
+                        return `http://${host}`;
+                });
+
+                return { sanboxId, sandboxUrl };
         },
 );
